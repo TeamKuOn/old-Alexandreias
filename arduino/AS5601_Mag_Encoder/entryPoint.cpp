09c9a139b322f88d5834ff7a780e09dfe103717a
xwasoux <oxwasouxo@gmail.com>
2022-07-23 18:24:56 +0900

    rename to 'main.cpp'

--- arduino/AS5601_Mag_Encoder/entryPoint.cpp
+++ arduino/AS5601_Mag_Encoder/entryPoint.cpp
@@ -10,11 +10,14 @@
 */
 
 #include "std_arduino.hpp"
+#include <Arduino_FreeRTOS.h>
 #include <stdint.h>
 #include <Wire.h>
 
 #define AS5600_AS5601_DEV_ADDRESS      0x36 // Register device address
 #define AS5600_AS5601_REG_RAW_ANGLE    0x0C // Register access address
+
+void getMagData(void *pvParameters);
 
 void setup() {
   // I2C init
@@ -24,21 +27,33 @@ void setup() {
   // Communication function init
   Serial.begin(115200);
   while (!Serial);
+
+  xTaskCreate(getMagData, 'getMagData', 128, NULL, 1, NULL);
+  
 }
 
 void loop() {
-  // Read RAW_ANGLE value from encoder
-  Wire.beginTransmission(AS5600_AS5601_DEV_ADDRESS);
-  Wire.write(AS5600_AS5601_REG_RAW_ANGLE);
-  Wire.endTransmission(false);
-  Wire.requestFrom(AS5600_AS5601_DEV_ADDRESS, 2);
+}
 
-  uint16_t RawAngle = 0;
-  RawAngle  = ((uint16_t)Wire.read() << 8) & 0x0F00;
-  RawAngle |= (uint16_t)Wire.read();
-  // Raw angle value (0x0000~0x0FFF) is stored in RawAngle
+void getMagData(void *pvParameters){
+  (void)pvParameters;
 
-  Serial.print("Raw angle value = ");
-  Serial.println(RawAngle);
-  delay(200);
+  for(;;){
+    // Read RAW_ANGLE value from encoder
+    Wire.beginTransmission(AS5600_AS5601_DEV_ADDRESS);
+    Wire.write(AS5600_AS5601_REG_RAW_ANGLE);
+    Wire.endTransmission(false);
+    Wire.requestFrom(AS5600_AS5601_DEV_ADDRESS, 2);
+
+    uint16_t RawAngle = 0;
+    RawAngle  = ((uint16_t)Wire.read() << 8) & 0x0F00;
+    RawAngle |= (uint16_t)Wire.read();
+    // Raw angle value (0x0000~0x0FFF) is stored in RawAngle
+
+    Serial.print("Raw angle value = ");
+    Serial.println(RawAngle);
+    delay(10);
+
+  }
+
 }
