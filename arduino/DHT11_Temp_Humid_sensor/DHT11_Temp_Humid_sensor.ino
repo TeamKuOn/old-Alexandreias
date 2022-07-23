99ac4162e21aba7d368123913d9e69329fdf929e
xwasoux <oxwasouxo@gmail.com>
2022-07-23 18:25:27 +0900

    add DHT11 temp&humid sensor

---
+++ arduino/DHT11_Temp_Humid_sensor/DHT11_Temp_Humid_sensor.ino
@@ -0,0 +1,5 @@
+/*
+ * Dammy File
+ * File:   arduino_with_cpp.ino
+ * Author: xwasoux
+ */
---
+++ arduino/DHT11_Temp_Humid_sensor/main.cpp
@@ -0,0 +1,35 @@
+/*
+ * main.cpp
+ */
+#include "std_arduino.hpp"
+#include "DHT.h"
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println(F("DHT11 test!"));
+
+    dht.begin();
+}
+
+void loop() {
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+
+    if (isnan(h) || isnan(t)) {
+        Serial.println(F("Failed to read from DHT sensor!"));
+        return;
+    }
+
+    Serial.print(F("Humidity: "));
+    Serial.print(h);
+    Serial.print(F("%  Temperature: "));
+    Serial.print(t);
+    Serial.println(F("°C "));
+
+    delay(1000);
+}
