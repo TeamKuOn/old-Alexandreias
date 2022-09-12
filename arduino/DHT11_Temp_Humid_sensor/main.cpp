8df38d80e1a1c7cc595c8832d013769e5cd041db
xwasoux <oxwasouxo@gmail.com>
2022-09-13 04:36:50 +0900

    implement with timer interrupt

--- arduino/DHT11_Temp_Humid_sensor/main.cpp
+++ arduino/DHT11_Temp_Humid_sensor/main.cpp
@@ -1,35 +1,37 @@
 /*
  * main.cpp
  */
-#include "std_arduino.hpp"
+#include "stdArduino.hpp"
+#include <Arduino_FreeRTOS.h>
 #include "DHT.h"
 
 #define DHTPIN 2
 #define DHTTYPE DHT11
 
+TimerHandle_t xMsec;
+
 DHT dht(DHTPIN, DHTTYPE);
+
+void xDHT11Callback(TimerHandle_t);
 
 void setup() {
     Serial.begin(115200);
     Serial.println(F("DHT11 test!"));
 
     dht.begin();
+
+    xMsec = xTimerCreate("DHT11", pdMS_TO_TICKS(100), pdTRUE, (void *)0, xDHT11Callback);
+    xTimerStart(xMsec, 0);
 }
 
-void loop() {
+void loop() {}
+
+void xDHT11Callback(TimerHandle_t xTime){
     float h = dht.readHumidity();
     float t = dht.readTemperature();
 
     if (isnan(h) || isnan(t)) {
         Serial.println(F("Failed to read from DHT sensor!"));
-        return;
     }
 
-    Serial.print(F("Humidity: "));
-    Serial.print(h);
-    Serial.print(F("%  Temperature: "));
-    Serial.print(t);
-    Serial.println(F("°C "));
-
-    delay(1000);
 }
